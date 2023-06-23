fdbfc96032c8d6e085bdd23237b5c6dac0cf2298
Fernando Santana <bh.carvalho@gmail.com>
2023-06-23 14:53:41 -0300

    router and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <NavBar/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
   </div>
+
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NavBar
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+
+nav {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  background-color: #009BDE;
+  padding: 10px;
+}
+
+nav a {
+  margin-right: 10px;
+  color: white;
+  text-decoration: none;
+}
+
+nav a:hover {
+  text-decoration: underline;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <center>
+            <h1 class="nav-header">
+                Post-List
+            </h1>
+        </center>
+        
+        <!-- <nav> -->
+            <b-navbar toggleable="md" type="blue" variant="blue">
+            <b-navbar-brand href="#">Menu</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                <b-nav-item href="#">Link 1</b-nav-item>
+                <b-nav-item href="#">Link 2</b-nav-item>
+                <b-nav-item href="#">Link 3</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+            </b-navbar>
+        
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+            <router-view></router-view>
+    
+        <!-- </nav> -->
+    </header>
+ 
+
+</template>
+
+<style>
+header {
+    padding:15px;
+    background:#009BDE;
+
+}
+header h1 {
+    color:#FFF;
+}
+</style>
